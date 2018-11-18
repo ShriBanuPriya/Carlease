210004fa431b0a96a41602a4f81fa62cd3144a54
ShriBanuPriya <ShriBanuPriya@users.noreply.github.com>
2018-11-18 12:57:46 +0530

    social icons changed in svg

--- js/script.js
+++ js/script.js
@@ -1,26 +1,92 @@
+
+       $(document).ready(function(){
+         $(".link-image1").hover(function(){
+           $('.first').show();
+           $('.third').hide();
+           $('.second').hide();
+           $('.fourth').hide();
+         });
+
+
+
+         $(".link-image2").hover(function(){
+           $('.second').show();
+           $('.third').hide();
+           $('.first').hide();
+           $('.fourth').hide();
+         });
+
+
+         $(".link-image3").hover(function(){
+           $('.third').show();
+           $('.second').hide();
+           $('.first').hide();
+           $('.fourth').hide();
+         });
+         $(".link-image4").hover(function(){
+           $('.fourth').show();
+           $('.third').hide();
+           $('.first').hide();
+           $('.second').hide();
+         });
+});
+
+
+
+
+
 jQuery(document).ready(function($) {
         		"use strict";
-        		//  TESTIMONIALS CAROUSEL HOOK
+
 		        $('#customers-testimonials').owlCarousel({
 		            loop: true,
-		            center: true,
+
 		            items: 3,
-		            margin: 0,
+		            margin: 20,
 		            autoplay: true,
 		            dots:true,
 		            autoplayTimeout: 8500,
 		            smartSpeed: 450,
+            
 		            responsive: {
-		              0: {
-		                items: 1
+                  0: {
+                    items: 1
+                  },
+                  600: {
+		                items: 1,
+                    center: true,
+                   margin: 30
 
 		              },
 		              768: {
-		                items: 2
+		                items: 1,
+                    margin: 50,
+                     autoWidth: true
+
 		              },
 		              1170: {
-		                items: 3
+		                items: 2,
+
+                    margin: 20
 		              }
 		            }
 		        });
         	});
+
+          $(document).ready(function(){
+        $(".link-image1").hover(function(){
+            $(".first-hover h3").toggleClass("hover-change");
+        });
+
+        $(".link-image2").hover(function(){
+            $(".second-hover h3").toggleClass("hover-change");
+        });
+
+        $(".link-image3").hover(function(){
+            $(".third-hover h3").toggleClass("hover-change");
+        });
+
+        $(".link-image4").hover(function(){
+            $(".fourth-hover h3").toggleClass("hover-change");
+        });
+    });
